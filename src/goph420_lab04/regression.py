2f00f0dbdd25f9aa287699047b4050774dcc03ae
Avneet Kaur <avneet.kaur3@ucalgary.ca>
2025-04-25 17:42:45 -0600

    added a general function for multiple linear least squares regression to regression.py

--- src/goph420_lab04/regression.py
+++ src/goph420_lab04/regression.py
@@ -14,8 +14,22 @@ def multi_regress(y, Z):
     -------
     numpy.ndarray, shape = (m,) or (m,1)
         The vector of model coefficients
-    numpy.ndarrat, shape = (n,) or (n,1)
+    numpy.ndarray, shape = (n,) or (n,1)
         The vector of residuals
     float
         The coefficient of determination, r^2
     """
+    #converting y to a column vector.
+    y = np.asarray(y).reshape(-1, 1)
+    Z = np.asarray(Z)
+    #similar to the example in class, creating a column of 1s and horizontally stacking it to Z to compute the coefficients.
+    Z = np.hstack([np.ones((Z.shape[0], 1)), Z])
+    #computing the coefficients according to the equation in lecture notes and lab manual.
+    coefficients = np.linalg.inv(Z.T @ Z) @ Z.T @ y
+    #computing the residuals.
+    e = y - (Z @ a)
+    #computing the coefficient of determination.
+    r_squared = 1 - (np.sum(e**2)/np.sum((y - np.mean(y))**2))
+
+    #returning the arrays and flattening them to ensure the correct shape is output - a one dimensional array.
+    return coefficients.flatten(), e.flatten(), r_squared
